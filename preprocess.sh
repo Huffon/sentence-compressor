e3ea3b877d3221f0daa0075af4e1eebb707cb115
huffon <huffonism@gmail.com>
2020-04-11 18:23:42 +0000

    Change preprocess bash

--- preprocess.sh
+++ preprocess.sh
@@ -1,46 +1,46 @@
-# git clone https://github.com/google-research-datasets/sentence-compression.git
-# mv sentence-compression/data data
+git clone https://github.com/google-research-datasets/sentence-compression.git
+mv sentence-compression/data data
 
-# wget https://www.jamesclarke.net/media/data/broadcastnews-compressions.tar.gz
-# tar xvzf broadcastnews-compressions.tar.gz 
+wget https://www.jamesclarke.net/media/data/broadcastnews-compressions.tar.gz
+tar xvzf broadcastnews-compressions.tar.gz 
 
-# wget https://www.jamesclarke.net/media/data/written-compressions.tar.gz
-# tar xvzf written-compressions.tar.gz
+wget https://www.jamesclarke.net/media/data/written-compressions.tar.gz
+tar xvzf written-compressions.tar.gz
 
-# wget https://download.microsoft.com/download/C/C/A/CCA10A93-4372-44CB-A2A3-B433ADB27276/Release.zip
-# mkdir Release
-# unzip Release.zip -d Release
+wget https://download.microsoft.com/download/C/C/A/CCA10A93-4372-44CB-A2A3-B433ADB27276/Release.zip
+mkdir Release
+unzip Release.zip -d Release
 
-# rm broadcastnews-compressions.tar.gz written-compressions.tar.gz Release.zip
+rm broadcastnews-compressions.tar.gz written-compressions.tar.gz Release.zip
 
-# python utils/preprocess.py
+python utils/preprocess.py
 
-# rm -rf annotator1 annotator2 annotator3 written Release data
+rm -rf annotator1 annotator2 annotator3 written Release
 
-# wget -N 'https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/encoder.json'
-# wget -N 'https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/vocab.bpe'
-# wget -N 'https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/dict.txt'
+wget -N 'https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/encoder.json'
+wget -N 'https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/vocab.bpe'
+wget -N 'https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/dict.txt'
 
-# for SPLIT in train val
-# do
-#   for LANG in source target
-#   do
-#     python -m examples.roberta.multiprocessing_bpe_encoder \
-#     --encoder-json encoder.json \
-#     --vocab-bpe vocab.bpe \
-#     --inputs "data/$SPLIT.$LANG" \
-#     --outputs "data/$SPLIT.bpe.$LANG" \
-#     --workers 60 \
-#     --keep-empty;
-#   done
-# done
+for SPLIT in train val
+do
+  for LANG in source target
+  do
+    python -m examples.roberta.multiprocessing_bpe_encoder \
+    --encoder-json encoder.json \
+    --vocab-bpe vocab.bpe \
+    --inputs "data/$SPLIT.$LANG" \
+    --outputs "data/$SPLIT.bpe.$LANG" \
+    --workers 60 \
+    --keep-empty;
+  done
+done
 
-# fairseq-preprocess \
-#     --source-lang source \
-#     --target-lang target \
-#     --trainpref data/train.bpe \
-#     --validpref data/val.bpe \
-#     --destdir data-bin \
-#     --workers 60 \
-#     --srcdict dict.txt \
-#     --tgtdict dict.txt
+fairseq-preprocess \
+    --source-lang source \
+    --target-lang target \
+    --trainpref data/train.bpe \
+    --validpref data/val.bpe \
+    --destdir data-bin \
+    --workers 60 \
+    --srcdict dict.txt \
+    --tgtdict dict.txt
--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,35 +1,14 @@
 """Data pre-processing function"""
 import os
-import glob
 import logging
 from typing import List
 
-
-def preprocess_edin(edin_dirs: List[str], source: List[str], target: List[str]):
-    """Preprocess Edinburgh's parallel sentence compression dataset
-    """
-    for edin_dir in edin_dirs:
-        for f_name in os.listdir(edin_dir):
-            with open(os.path.join(edin_dir, f_name), "r", encoding="utf-8") as f_p:
-                lines = f_p.readlines()
-                for line in lines:
-                    line = line.strip()
-                    s = line.find('>')
-                    e = line.rfind('<')
-                    if line.startswith("<original"):
-                        source.append(line[s+1:e])
-                    elif line.startswith("<compressed"):
-                        target.append(line[s+1:e])
-
-    logging.info(f"Final dataset size: {len(source)}")
-
-    return source, target
+import pandas as pd
 
 
 def preprocess_google(mode: str, prefix: str, nums: List[str]):
     """Preprocess JSON containing parallel sentence compression data
     """
-    logging.info(f"[{mode.upper()}]")
     src, tgt = list(), list()
 
     for i, num in enumerate(nums):
@@ -59,15 +38,62 @@ def preprocess_google(mode: str, prefix: str, nums: List[str]):
     return src, tgt
 
 
+def preprocess_edin(edin_dirs: List[str], source: List[str], target: List[str]):
+    """Preprocess Edinburgh's parallel sentence compression dataset
+    """
+    for edin_dir in edin_dirs:
+        for f_name in os.listdir(edin_dir):
+            with open(os.path.join(edin_dir, f_name), "r", encoding="utf-8") as f_p:
+                lines = f_p.readlines()
+
+                for line in lines:
+                    line = line.strip()
+
+                    s = line.find('>')
+                    e = line.rfind('<')
+
+                    if line.startswith("<original"):
+                        source.append(line[s+1:e])
+                    elif line.startswith("<compressed"):
+                        target.append(line[s+1:e])
+
+    logging.info(f"Current dataset size: {len(source)}")
+
+    return source, target
+
+
+def preprocess_msr(source: List[str], target: List[str]):
+    """Preprocess MSR parallel sentence compression dataset
+    """
+    msr = "Release/RawData"
+    datasets = ["train", "valid", "test"]
+
+    for dataset in datasets:
+        dataframe = pd.read_csv(os.path.join(msr, f"{dataset}.tsv"), delimiter='\t', header=None, error_bad_lines=False)
+        
+        for item in dataframe[2].iteritems():
+            splitted = item[1].split("|||")
+
+            src, tgt = splitted[0], splitted[1]
+            tgt = tgt.split("\t")[0]
+
+            source.append(src.strip())
+            target.append(tgt.strip())
+
+    logging.info(f"Current dataset size: {len(source)}")
+
+    return source, target
+
+
 def create_pair(mode: str, src: List[str], tgt: List[str]):
     """Create source and target file using pre-generated list
     """
-    with open(f"data/{mode}.src", "w", encoding="utf-8") as f_src:
+    with open(f"data/{mode}.source", "w", encoding="utf-8") as f_src:
         for source in src:
             f_src.write(source)
             f_src.write("\n")
 
-    with open(f"data/{mode}.tgt", "w", encoding="utf-8") as f_tgt:
+    with open(f"data/{mode}.target", "w", encoding="utf-8") as f_tgt:
         for target in tgt:
             f_tgt.write(target)
             f_tgt.write("\n")
@@ -75,20 +101,24 @@ def create_pair(mode: str, src: List[str], tgt: List[str]):
 
 def main():
     """Main function"""
-    # prefix = "data/sent-comp.train"
-    # nums = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"]
-    # src, tgt = preprocess_google("train", prefix, nums)
+    logging.info("[TRAIN] Google Dataset")
+    prefix = "data/sent-comp.train"
+    nums = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"]
+    src, tgt = preprocess_google("train", prefix, nums)
 
-    src, tgt = [], []
+    logging.info("[TRAIN] Edinburgh Dataset")
     edin_dirs = ["annotator1", "annotator2", "annotator3", "written"]
     src, tgt = preprocess_edin(edin_dirs, src, tgt)
-    create_pair("train", [], [])
 
-    # prefix = "data/comp-data.eval"
-    # nums = [""]
-    # src, tgt = preprocess_google("val", prefix, nums)
-    # create_pair("eval", src, tgt)
+    logging.info("[TRAIN] MSR Dataset")
+    src, tgt = preprocess_msr(src, tgt)
+    create_pair("train", src, tgt)
 
+    logging.info("[EVAL] Google Dataset")
+    prefix = "data/comp-data.eval"
+    nums = [""]
+    src, tgt = preprocess_google("val", prefix, nums)
+    create_pair("val", src, tgt)
 
 
 if __name__ == "__main__":
