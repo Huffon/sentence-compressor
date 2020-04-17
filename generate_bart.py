f76907d3b7afa4500246fc8c6e82f276f4c0c2b0
huffon <huffonism@gmail.com>
2020-04-17 10:40:27 +0000

    Modify print option

--- generate_bart.py
+++ generate_bart.py
@@ -11,16 +11,13 @@ def main():
 
     with open('output/input.txt') as source:
         lines = [line.replace("\n", "") for line in source.readlines()]
-
-        print("[Before]")
-        for i, line in enumerate(lines):
-            print(f"({i+1}): {line}")
         
         with torch.no_grad():
             preds = bart.sample(lines, beam=4, lenpen=2.0, no_repeat_ngram_size=2, temperature=0.9)
-            print("\n[After]")
-            for i, pred in enumerate(preds):
-                print(f"({i+1}): {pred}")
+            for i, (line, pred) in enumerate(zip(lines, preds)):
+                print(f"[ori] ({i+1}): {line}")
+                print(f"[com] ({i+1}): {pred}")
+                print()
 
 
 if __name__ == "__main__":
